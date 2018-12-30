316bb95fc79ea5ea283d77b007443cf18f9e022e
DinoSaulo <sab2@cin.ufpe.br>
2018-12-30 18:41:43 -0300

    conversão para + adicionada

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    let x = document.getElementById("myTextarea").value;
+    let termosDeBuscaSeparados = []
+    termosDeBuscaSeparados = x.split(/[\n|\n\r]/)
+    let  finalArray = []
+
+    for(let i = 0; i < termosDeBuscaSeparados.length; i++) {
+        let palavrasDosTermosDeBuscaSeparados = termosDeBuscaSeparados[i].split(' ')
+
+        for(let j = 0; j < palavrasDosTermosDeBuscaSeparados.length; j++) {
+            finalArray += ('+').concat(palavrasDosTermosDeBuscaSeparados[j]).concat(' ')
+            if( j === palavrasDosTermosDeBuscaSeparados.length - 1) finalArray = finalArray.concat('</br>')   
+        }
+    }
+
+    document.getElementById("demo").innerHTML = finalArray;
+}
