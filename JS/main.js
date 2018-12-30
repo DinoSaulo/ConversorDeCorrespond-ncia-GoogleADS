14a41ff579093583b255f8c50c58ca47f4500e78
DinoSaulo <sab2@cin.ufpe.br>
2018-12-30 18:56:21 -0300

    todas as funções de correspondência implementadas

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-function addMaisa() {
+function addMais() {
     let x = document.getElementById("myTextarea").value;
     let termosDeBuscaSeparados = []
     termosDeBuscaSeparados = x.split(/[\n|\n\r]/)
@@ -9,6 +9,77 @@ function addMaisa() {
 
         for(let j = 0; j < palavrasDosTermosDeBuscaSeparados.length; j++) {
             finalArray += ('+').concat(palavrasDosTermosDeBuscaSeparados[j]).concat(' ')
+            if( j === palavrasDosTermosDeBuscaSeparados.length - 1) finalArray = finalArray.concat('</br>')   
+        }
+    }
+
+    document.getElementById("demo").innerHTML = finalArray;
+}
+
+function addNormal() {
+    let x = document.getElementById("myTextarea").value;
+    document.getElementById("demo").innerHTML = x;
+}
+
+function addAspas() {
+    let x = document.getElementById("myTextarea").value;
+    let termosDeBuscaSeparados = []
+    termosDeBuscaSeparados = x.split(/[\n|\n\r]/)
+    let finalArray = ''
+
+    for(let i = 0; i < termosDeBuscaSeparados.length; i++) {
+        let palavrasDosTermosDeBuscaSeparados = termosDeBuscaSeparados[i].split(' ')
+        
+        for(let j = 0; j < palavrasDosTermosDeBuscaSeparados.length; j++) {
+            if( j === 0) finalArray = finalArray.concat('"') 
+            
+            if( j === palavrasDosTermosDeBuscaSeparados.length - 1) {
+                finalArray = finalArray.concat(palavrasDosTermosDeBuscaSeparados[j])
+                finalArray = finalArray.concat('"</br>')
+            } else {
+                finalArray = finalArray.concat(palavrasDosTermosDeBuscaSeparados[j]).concat(' ')
+            }
+        }
+    }
+
+    document.getElementById("demo").innerHTML = finalArray;
+}
+
+function addChaves() {
+    let x = document.getElementById("myTextarea").value;
+    let termosDeBuscaSeparados = []
+    termosDeBuscaSeparados = x.split(/[\n|\n\r]/)
+    let finalArray = ''
+
+    for(let i = 0; i < termosDeBuscaSeparados.length; i++) {
+        let palavrasDosTermosDeBuscaSeparados = termosDeBuscaSeparados[i].split(' ')
+        
+        for(let j = 0; j < palavrasDosTermosDeBuscaSeparados.length; j++) {
+            if( j === 0) finalArray = finalArray.concat('[') 
+            
+            if( j === palavrasDosTermosDeBuscaSeparados.length - 1) {
+                finalArray = finalArray.concat(palavrasDosTermosDeBuscaSeparados[j])
+                finalArray = finalArray.concat(']</br>')
+            } else {
+                finalArray = finalArray.concat(palavrasDosTermosDeBuscaSeparados[j]).concat(' ')
+            }
+        }
+    }
+
+    document.getElementById("demo").innerHTML = finalArray;
+}
+
+function addMenos() {
+    let x = document.getElementById("myTextarea").value;
+    let termosDeBuscaSeparados = []
+    termosDeBuscaSeparados = x.split(/[\n|\n\r]/)
+    let  finalArray = []
+
+    for(let i = 0; i < termosDeBuscaSeparados.length; i++) {
+        let palavrasDosTermosDeBuscaSeparados = termosDeBuscaSeparados[i].split(' ')
+
+        for(let j = 0; j < palavrasDosTermosDeBuscaSeparados.length; j++) {
+            finalArray += ('-').concat(palavrasDosTermosDeBuscaSeparados[j]).concat(' ')
             if( j === palavrasDosTermosDeBuscaSeparados.length - 1) finalArray = finalArray.concat('</br>')   
         }
     }
